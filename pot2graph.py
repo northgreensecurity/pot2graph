28e1a102e640060a56c2e91f9e98caf7c560d14b
North Green Security <108965118+NorthGreenSecurity@users.noreply.github.com>
2023-01-17 01:13:23 +0000

    Create pot2graph.py

---
+++ pot2graph.py
@@ -0,0 +1,40 @@
+import csv
+import re
+import matplotlib.pyplot as plt
+
+# Ask user for Potfile
+potfile_name = input("Please enter the name of the potfile to be analysed: ")
+
+# Open Potfile
+with open(potfile_name, "r") as f:
+    potfile = f.readlines()
+
+
+# Create a dictionary to store the data
+data = {}
+
+# Iterate through the potfile
+for line in potfile:
+    # Split the line into the hash and password
+    hash, password = line.strip().split(":")
+    length = len(password)
+    if length in data:
+        data[length] += 1
+    else:
+        data[length] = 1
+
+# Create the pie chart
+labels = list(data.keys())
+sizes = list(data.values())
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+plt.title("Distribution of Password Lengths in Potfile")
+plt.savefig("potfile_graph.jpg")
+plt.show()
+
+# Write the data to a CSV file
+with open("potfile_data.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Length", "Count"])
+    for key, value in data.items():
+        writer.writerow([key, value])
+
